15deacc05e8c865379fdd75220a88745e091d277
noah_rowe <noahrowe@protonmail.com>
2024-07-04 10:30:20 -0230

    infinite combination of invisible widgets to overcome that damned white screen

--- lib/login/welcome_screen.dart
+++ lib/login/welcome_screen.dart
@@ -20,28 +20,32 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-                  child: ElevatedButton(
-                    child: const Text(
-                      'Log In',
-                      style: TextStyle(fontSize: 15),
-                    ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, 'login_screen');
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-                  child: ElevatedButton(
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+                    child: ElevatedButton(
                       child: const Text(
-                        'Sign up',
+                        'Log In',
                         style: TextStyle(fontSize: 15),
                       ),
                       onPressed: () {
-                        Navigator.pushNamed(context, 'registration_screen');
-                      }),
+                        Navigator.pushNamed(context, 'login_screen');
+                      },
+                    ),
+                  ),
+                ),
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+                    child: ElevatedButton(
+                        child: const Text(
+                          'Sign up',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        onPressed: () {
+                          Navigator.pushNamed(context, 'registration_screen');
+                        }),
+                  ),
                 ),
               ]),
         ),
